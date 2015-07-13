c726b924e76ffe8b7ea2e62b5f66cf7ed03d4d7f
madctol <madctol@gmail.com>
2015-07-13 19:59:43 +0200

    se simplifica el código

--- random.js
+++ random.js
@@ -1,8 +1,7 @@
 var numero = Math.random();
 
+var str = 'MAYOR que 0,5';
+
 if (numero <= 0.5){
 	console.log('\n' + numero + ' MENOR que 0,5 \n');
-}
-else {
-	console.log('\n' + numero + ' MAYOR que 0,5 \n');
 }
